c9fe51dc04f6b53fcf11735560f986f6715ea95b
Arnanz <pashamaulana14@gmail.com>
2024-10-05 11:30:52 +0700

    Done

---
+++ Pemrograman Web/Pertemuan6/array1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h2>Array Terindeks</h2>
+<?php
+$ListDosen=["Elok Nur Hamdana","Unggul Pamenang","Bagas Nugraha"];
+
+$urutan = array(2, 0, 1);
+foreach ($urutan as $i) {
+    echo $ListDosen[$i] . "<br>";
+}
+?>
+</body>
+</html>
